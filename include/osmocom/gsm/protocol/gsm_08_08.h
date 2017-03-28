0d7b72464071cbeaca00b08146058c6b7f9e35a3
Philipp Maier <pmaier@sysmocom.de>
2017-03-28 17:05:40 +0200

    gsm0808: Add utils for Channel Type
    The planned support for true A over IP requires the encoding of
    the a Channel Type element (see also ASSIGNMENT REQUEST).
    
    This commt adds encoding/decoding functionality and tests for
    the element mentioned above, however, it is not yet actively used.

--- include/osmocom/gsm/protocol/gsm_08_08.h
+++ include/osmocom/gsm/protocol/gsm_08_08.h
@@ -433,3 +433,12 @@ struct gsm0808_speech_codec {
 	bool type_extended;
 	bool cfg_present;
 };
+
+/* 3GPP TS 48.008 3.2.2.11 Channel Type */
+#define CH_TYPE_PERM_SPCH_MAXLEN 9
+struct gsm0808_channel_type {
+	uint8_t ch_indctr;
+	uint8_t ch_rate_type;
+	uint8_t perm_spch[CH_TYPE_PERM_SPCH_MAXLEN];
+	unsigned int perm_spch_len;
+};
